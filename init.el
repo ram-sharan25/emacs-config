08b330c8e2246117f673bb2b08261bc58acd821f
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2024-12-22 15:59:19 -0600

    Add comment in whole line

--- init.el
+++ init.el
@@ -1,3 +1,12 @@
+(cond ((eq system-type 'windows-nt)
+       (add-to-list 'package-archives
+		    '("melpa" . "http://melpa.org/packages/") t))
+      (t
+       (add-to-list 'package-archives
+		    '("melpa" . "https://melpa.org/packages/") t)))
+(package-initialize)
+
+
 (setf custom-file (expand-file-name ".custom" user-emacs-directory))
 
 ;; Add the modules/personal directory to the load path
@@ -6,6 +15,13 @@
 
 (define-prefix-command 'bp/global-prefix-map)
 (define-key global-map (kbd "M-m") 'bp/global-prefix-map)
+
+;; Install tree-sitter grammar for typescript and tsx
+
+
+
+
+
 
 ;; Function to load all .el files from a directory
 (defun load-directory (directory)
@@ -19,16 +35,6 @@
 
 
 
-(cond ((eq system-type 'windows-nt)
-       (add-to-list 'package-archives
-		    '("melpa" . "http://melpa.org/packages/") t))
-      (t
-       (add-to-list 'package-archives
-		    '("melpa" . "https://melpa.org/packages/") t)))
-(package-initialize)
-
-
-
 
 
 
@@ -37,12 +43,8 @@
   :bind ("C-s" . swiper))
 
 
-
 ;; show lines in all modes
 (global-display-line-numbers-mode 1)
-
-
-
 
 
 (setq org-src-fontify-natively t)  ;; Enable syntax highlighting for source code blocks
