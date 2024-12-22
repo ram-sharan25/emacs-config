0c99504ca2192be6479ac7b17158090c251dfa44
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2024-12-22 16:10:18 -0600

    add theme in html exports

---
+++ modules/personal/org-config.el
@@ -0,0 +1,13 @@
+(setq org-html-head-include-default-style nil)
+ "#+setupfile: /Users/rrimal/.emacs.d/modules/git clones/src/comfy_inline/comfy_inline.theme"
+
+(defun bp/org-publish--add-setupfile (&rest args)
+  (goto-char (point-min))
+  (search-forward "#+title")
+  (beginning-of-line)
+  (insert "#+setupfile: /Users/rrimal/.emacs.d/modules/git clones/src/comfy_inline/comfy_inline.theme\n"))
+
+(use-package ox
+  :defer t
+  :config
+  (add-hook 'org-export-before-processing-functions #'bp/org-publish--add-setupfile))
