3f0bd399b7c2fb70c44fbe6d752b494ef3f3ef3b
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-01-02 01:18:33 -0600

    fix image saving in org-mpv notes

--- modules/personal/mpv-notes.el
+++ modules/personal/mpv-notes.el
@@ -4,10 +4,10 @@
 
 (use-package org-mpv-notes
   :ensure t
-  :after org-download
   :commands (org-mpv-notes-mode org-mpv-notes-open)
   :hook (org-mode . org-mpv-notes-setup-link)
   :config
+  (require 'org-download)
   (setq org-mpv-notes-save-image-function #'org-download-image)
   (define-key org-mpv-notes-mode-map (kbd "M-n") (smartrep-map org-mpv-notes-key-bindings)))
 
