c5b463682a65bde22826932c9f44ba4fb993422d
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-02-10 15:32:02 -0600

    add perspective in the workflow

---
+++ modules/personal/perspective-config.el
@@ -0,0 +1,7 @@
+(use-package perspective
+  :bind
+  ("C-x C-b" . persp-list-buffers)         ; or use a nicer switcher, see below
+  :custom
+  (persp-mode-prefix-key (kbd "C-c C-p"))  ; pick your own prefix key here
+  :init
+  (persp-mode))
