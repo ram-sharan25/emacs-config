bbdd1b3663b2015a67e413c0bc68f76a68902d6a
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-02-11 14:47:35 -0600

    modify helm config

--- modules/personal/helm-config.el
+++ modules/personal/helm-config.el
@@ -1,22 +1,26 @@
-;;helm package for emacs
-
+;; Helm Dash
 (use-package helm-dash
   :ensure t
   :defer t
   :config
   (setq helm-dash-docsets-path
-	(case system-type
-	  (linux "/mnt/Data/Files/Documentation/docsets/")
-	  (darwin "~/Library/Application Support/Dash/DocSets/"))
+	(cond
+	 ((eq system-type 'gnu/linux) "/mnt/Data/Files/Documentation/docsets/")
+	 ((eq system-type 'darwin) "~/Library/Application Support/Dash/DocSets/"))
 	dash-docs-enable-debugging nil
 	helm-dash-browser-func 'browse-web)
-  (bind-keys :map bp/global-prefix-map
-	     ("d h" . helm-dash)
-	     ("d ." . helm-dash-at-point)))
 
+  ;; Define a sub-keymap for "d"
+  (define-prefix-command 'rsr/doc-map)
+  (define-key rsr/global-prefix-map (kbd "d") 'rsr/doc-map)
+
+  (define-key rsr/doc-map (kbd "h") 'helm-dash)
+  (define-key rsr/doc-map (kbd ".") 'helm-dash-at-point))
+
+;; Helm Recoll
 (use-package helm-recoll
   :ensure t
   :defer t
-  :init
-  (setf helm-recoll-options '("/Applications/recoll.app/Contents/MacOS/recoll" "-t"))
+  :config
+  (setq helm-recoll-options '("/Applications/recoll.app/Contents/MacOS/recoll" "-t"))
   (helm-recoll-create-source "main" "~/.recoll"))
