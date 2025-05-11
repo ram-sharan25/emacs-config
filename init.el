c87a6c5a156d20a37b196f3dcea1c1a2ecd7f6bd
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-05-11 01:17:51 -0500

    revamp init and add code block evaluation in org file

--- init.el
+++ init.el
@@ -1,11 +1,26 @@
+(require 'package)
+(package-initialize)  ;; Ensure packages are initialized
+
+;; Add MELPA repository
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Install `use-package` if not installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; Always ensure packages are installed
+(setq use-package-always-ensure t)
+
 (setf custom-file (expand-file-name ".custom" user-emacs-directory))
 
 (push "/Users/rrimal/.local/bin" exec-path)
 (setenv "PATH" (concat "/Users/rrimal/.local/bin/:" (getenv "PATH")))
 
-;; Add the modules/personal directory to the load path
+;; Add personal directory to load path
 (add-to-list 'load-path (expand-file-name "modules/personal" user-emacs-directory))
-
 
 (define-prefix-command 'rsr/global-prefix-map)
 (define-key global-map (kbd "M-m") 'rsr/global-prefix-map)
@@ -18,27 +33,23 @@
       (message "Loading %s" file)
       (load (file-name-sans-extension file)))))
 
-;; Load all .el files from the personal directory
-(load-directory (expand-file-name "modules/personal" user-emacs-directory))
-
-
-
-(cond ((eq system-type 'windows-nt)
-       (add-to-list 'package-archives
-		    '("melpa" . "http://melpa.org/packages/") t))
-      (t
-       (add-to-list 'package-archives
-		    '("melpa" . "https://melpa.org/packages/") t)))
-(package-initialize)
-
-
-
+;; Install & configure swiper
 (use-package swiper
-  :ensure t
   :bind ("C-s" . swiper))
 
 (setq org-src-fontify-natively t)  ;; Enable syntax highlighting for source code blocks
-
 (global-font-lock-mode 1)
-
 (set-face-attribute 'default nil :height 160)
+
+;; Load custom modules AFTER setting up `use-package`
+(load-directory (expand-file-name "modules/personal" user-emacs-directory))
+
+(setq dired-use-ls-dired nil)
+
+(require 'org)
+(setq org-babel-python-command "python3") ; Or "python"
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (shell . t)))
+(setq org-confirm-babel-evaluate nil)
