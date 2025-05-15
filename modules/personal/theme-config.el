1a5d1de7c29d97a712c8a15e49503c09bde8d1e0
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-05-15 13:52:34 -0500

    change theme

--- modules/personal/theme-config.el
+++ modules/personal/theme-config.el
@@ -1,9 +1,3 @@
-;;for solarized theme
-
 (add-to-list 'custom-theme-load-path "~/./.emacs.d/theme/emacs-color-theme-solarized")
 (load-theme 'solarized t)
 (solarized-update-background-mode 'dark)
-;(add-hook 'after-make-frame-functions
-         ; (lambda (frame)
-          ;  (solarized-update-background-mode 'dark)
-          ;   ))
