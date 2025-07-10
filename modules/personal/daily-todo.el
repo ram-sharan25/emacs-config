c8caf8c625f76511825106e6d228ac366051fbaa
Ram Sharan Rimal <rsr0012@uah.edu>
2025-07-09 22:51:25 -0500

    modify the daily setup file

--- modules/personal/daily-todo.el
+++ modules/personal/daily-todo.el
@@ -2,10 +2,41 @@
 
 (require 'org)
 (require 'org-capture)
+(require 'org-id)
 
 (setq personal-dir "~/Stillness/Personal/Writings/")
  ;;"Base directory for personal writings."
 (setq diary-file (concat personal-dir "Diary.org"))
+(setq daily-file (concat personal-dir "Journal.org"))
+
+
+
+
+
+(defun ensure-date-tree ()
+  "Create the year/month/day heading structure for today if it doesn't exist."
+  (let ((year (format-time-string "%Y"))
+	(month (format-time-string "%Y-%m %B"))
+	(day (format-time-string "%Y-%m-%d %A")))
+    (save-excursion
+      (goto-char (point-min))
+      ;; Find or create Year headline
+      (unless (re-search-forward (format "^\\* %s$" year) nil t)
+	(goto-char (point-max))
+	(insert (format "\n* %s" year)))
+      ;; Find or create Month headline
+      (goto-char (point-min))
+      (re-search-forward (format "^\\* %s$" year))
+      (unless (re-search-forward (format "^\\** %s$" month) nil t)
+	(org-end-of-subtree)
+	(insert (format "\n** %s" month)))
+      ;; Find or create Day headline
+      (goto-char (point-min))
+      (re-search-forward (format "^\\* %s$" year))
+      (re-search-forward (format "^** %s$" month))
+      (unless (re-search-forward (format "^\\*** %s$" day) nil t)
+	(org-end-of-subtree)
+	(insert (format "\n*** %s" day))))))
 
 
 (defun diary--now ()
@@ -17,23 +48,34 @@
 (setq org-capture-templates
       `(("t" "TODO Entry" entry
 	 (file ,diary-file)
-	 "* TODO %^{Title} :TODO:\n:PROPERTIES:\n:NAME: %\\1\n:TIME: %(diary--now)\n:END:\n- Description: %?\n- DONE:"
+	 "* TODO %^{Title} :TODO:\n :ID:    %(org-id-new)\n :PROPERTIES:\n:NAME: %\\1\n:TIME: %(diary--now)\n:END:\n- Description: %?\n- DONE:"
 	 :empty-lines 1)
 
 	("m" "Meeting Entry" entry
 	 (file ,diary-file)
-	 "* MEETING with %^{Person} :MEETING:\n:PROPERTIES:\n:NAME: %\\1\n:TIME: %(diary--now)\n:END:\n- Agenda: %^{Agenda}\n- Discussions:\n  - %?"
-	 :empty-lines 1)
-
-	("j" "Journal Entry" entry
-	 (file ,diary-file)
-	 "* Journal :JOURNAL:\n:PROPERTIES:\n:TIME: %(diary--now)\n:END:\n%?"
+	 "* MEETING with %^{Person} :MEETING:\n:ID:    %(org-id-new)\n :PROPERTIES:\n:NAME: %\\1\n:TIME: %(diary--now)\n:END:\n- Agenda: %^{Agenda}\n- Discussions:\n  - %?"
 	 :empty-lines 1)
 
 	("i" "Idea Entry" entry
 	 (file ,diary-file)
-	 "* %^{Title} :IDEA:\n:PROPERTIES:\n:NAME: %\\1\n:TIME: %(diary--now)\n:END:\n- Description: %?"
-	 :empty-lines 1)))
+	 "* %^{Title} :IDEA:\n:ID:    %(org-id-new)\n:PROPERTIES:\n:NAME: %\\1\n:TIME: %(diary--now)\n:END:\n- Description: %?"
+	 :empty-lines 1)
+
+	("u" "Scratch Note " entry
+	 (file ,diary-file)
+	 "* Small Note  %^{Title} :NOTE:\n:ID:    %(org-id-new)\n:PROPERTIES:\n:TOPIC: %\\1\n:TIME: %(diary--now)\n:END:\n- Description: %?"
+	 :empty-lines 1)
+
+	("j" "Journal Entry" plain ; Use 'plain' type to insert text directly
+	 (file+function ,daily-file
+			(lambda ()
+			  ;; Navigate to the end of today's entry
+			  (let ((day-heading (format-time-string "^\\*\\*\\* %Y-%m-%d")))
+			    (goto-char (point-min))
+			    (re-search-forward day-heading nil t)
+			    (org-end-of-subtree))))
+	 ;; This template creates a list item instead of a new headline
+	 "**** %<%I:%M %p>:\n - %? \n - [[%F][source]]")))
 
 
 
@@ -44,7 +86,7 @@
 
 (defun diary--collect-entries (&optional only-today)
   "Return an alist of (tag . list-of-entries). If ONLY-TODAY is non-nil, filter by today's date."
-  (let ((tags '("TODO" "MEETING" "JOURNAL" "IDEA"))
+  (let ((tags '("TODO" "MEETING"  "IDEA"))
 	(entries (make-hash-table :test #'equal))
 	(today (format-time-string "%Y-%m-%d")))
     (with-current-buffer (find-file-noselect diary-file)
@@ -86,7 +128,7 @@
 
 (defun diary--get-tagged-entries-today ()
   "Return an alist of (tag . parsed-entry) for today's entries."
-  (let ((tags '("TODO" "MEETING" "JOURNAL" "IDEA"))
+  (let ((tags '("TODO" "MEETING"  "IDEA"))
 	(today (diary--today-date))
 	(result (make-hash-table :test #'equal)))
     (with-current-buffer (find-file-noselect diary-file)
@@ -124,13 +166,31 @@
   (let ((entries (diary--collect-entries nil)))
     (diary--display-grouped-entries entries "*Diary: All Entries by Tag*")))
 
+(advice-add 'org-capture :before
+	    (lambda (&rest _)
+	      (with-current-buffer (find-file-noselect daily-file)
+		(ensure-date-tree))))
+
+
+(defun open-daily-file ()
+  "Open daily file and jump to today's entry."
+  (interactive)
+  (find-file daily-file)
+  (ensure-date-tree)
+  (let ((day-heading (format-time-string "^\\*\\*\\* %Y-%m-%d")))
+    (goto-char (point-min))
+    (re-search-forward day-heading nil t)))
+
+
+
 ;;; === Keybindings ===
 
 (global-set-key (kbd "C-c t") (lambda () (interactive) (org-capture nil "t")))  ;; New TODO
 (global-set-key (kbd "C-c m") (lambda () (interactive) (org-capture nil "m")))  ;; New Meeting
-(global-set-key (kbd "C-c j") (lambda () (interactive) (org-capture nil "j")))  ;; New Journal
 (global-set-key (kbd "C-c i") (lambda () (interactive) (org-capture nil "i")))  ;; New Idea
-
+(global-set-key (kbd "C-c j") (lambda () (interactive) (org-capture nil "j")))
+(global-set-key (kbd "C-c u") (lambda () (interactive) (org-capture nil "u")))
+(global-set-key (kbd "C-c o d") 'open-daily-file)
 (global-set-key (kbd "C-c d") #'diary-list-entries-for-today)
 (global-set-key (kbd "C-c l") #'diary-list-all-entries-by-tag)
 
