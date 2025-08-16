29c322dc7f2c7ba1c70b41ed4fe5db137ada266f
Ram Sharan Rimal <rsr0012@uah.edu>
2025-08-16 18:11:55 -0500

    change theme for macos

--- modules/personal/theme-config.el
+++ modules/personal/theme-config.el
@@ -1,16 +1,21 @@
-;;(add-to-list 'custom-theme-load-path "~/./.emacs.d/theme/emacs-color-theme-solarized")
+;; Add the DIRECTORY containing the theme file to the load path
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") ;; Assumes this is the correct path
+
+;; Now, load the theme by its symbol name
+(load-theme 'timu-macos t)
+;;(load-theme ../../themes/mac-os-theme.el t)
 
 
-(use-package gruvbox-theme
-  :ensure t
-  :config
-  (load-theme 'gruvbox-dark-medium t)
+;; (use-package gruvbox-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'gruvbox-dark-medium t)
 
-  ;; Softer "white" for main text
-  (set-face-attribute 'default nil :foreground "#dddddd")
+;;   ;; Softer "white" for main text
+;;   (set-face-attribute 'default nil :foreground "#dddddd")
 
-  ;; Optional: keep comments dim, but readable
-  (custom-set-faces
-   '(font-lock-comment-face ((t (:foreground "#928374"))))
-   '(font-lock-string-face  ((t (:foreground "#b8bb26")))))
-)
+;;   ;; Optional: keep comments dim, but readable
+;;   (custom-set-faces
+;;    '(font-lock-comment-face ((t (:foreground "#928374"))))
+;;    '(font-lock-string-face  ((t (:foreground "#b8bb26")))))
+;; )
