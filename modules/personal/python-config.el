61c631918ea61b14da15e3ab433c969699327ba4
Ram Sharan Rimal <rsr0012@uah.edu>
2025-08-20 03:27:06 -0500

    add lsp configs for python

--- modules/personal/python-config.el
+++ modules/personal/python-config.el
@@ -1,4 +1,3 @@
-
 ;;python3 configs
 ;; Load support for Python code blocks
 
@@ -16,3 +15,26 @@
 
 (set-face-attribute 'default nil :height 160)
 
+
+(with-eval-after-load 'lsp-mode
+  ;; Point to the LSP server executable inside your new virtual environment
+  (setq lsp-pyright-executable "~/.emacs.d/pyvenv/bin/pylsp"))
+
+;; --- Python Support ---
+;; To install the server, run: pip install 'python-lsp-server[all]'
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+			  (require 'lsp-pyright)
+			  (lsp-deferred))))
+
+;; --- C/C++ Support ---
+;; To install the server (e.g., on macOS): brew install ccls
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode) . (lambda ()
+			       (require 'ccls)
+			       (lsp-deferred))))
+
+
+(provide 'lsp-languages)
