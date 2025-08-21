f769fa9e111b95a93cd9a853dd20686ed94884a9
Ram Sharan Rimal <rsr0012@uah.edu>
2025-08-20 21:34:03 -0500

    fix lsp mode for doc view

--- modules/personal/lsp_mode.el
+++ modules/personal/lsp_mode.el
@@ -6,24 +6,31 @@
   :config
   (setq lsp-auto-guess-root t)
   (setq lsp-project-root-patterns
-	'("package.json" "tsconfig.json" "jsconfig.json" ".git" ".project" ".projectile"))
+  '("package.json" "tsconfig.json" "jsconfig.json" ".git" ".project" ".projectile"))
 
   ;; PERFORMANCE
   (setq lsp-completion-provider :capf
-	lsp-idle-delay 0.1
-	lsp-document-sync-method 'incremental
-	lsp-response-timeout 10
-	lsp-enable-file-watchers t
-	lsp-file-watch-threshold 5000)
+  lsp-idle-delay 0.1
+  lsp-document-sync-method 'incremental
+  lsp-response-timeout 10
+  lsp-enable-file-watchers t
+  lsp-file-watch-threshold 5000)
 
   ;; INTEGRATIONS
   (setq lsp-eslint-enable t))
+  (setq lsp-format-buffer-on-save nil)
+  ;; This sets the default for all buffers to use spaces instead of tabs.
+  (setq-default indent-tabs-mode nil)
+
+  ;; This sets the visual width of a tab to 2 spaces. This is useful
+  ;; for files that might still contain tab characters.
+  (setq-default tab-width 2)
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
   :bind
   (:map lsp-ui-mode-map
-	("C-c v d" . lsp-ui-doc-show))
+  ("C-c v d" . lsp-ui-doc-show))
   :config
   ;; DOCUMENTATION SETTINGS
   (setq lsp-ui-doc-show-with-mouse nil)
@@ -33,8 +40,8 @@
 
   ;; OTHER UI FEATURES
   (setq lsp-ui-peek-enable t
-	lsp-ui-imenu-enable t
-	lsp-ui-sideline-enable t
-	lsp-ui-sideline-delay 0.1))
+  lsp-ui-imenu-enable t
+  lsp-ui-sideline-enable t
+  lsp-ui-sideline-delay 0.1))
 
 (provide 'lsp-config)
