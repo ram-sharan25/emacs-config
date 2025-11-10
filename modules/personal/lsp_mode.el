0ab5cf20dcaa3f4600f113a51de9ad413c75973b
Ram Sharan Rimal <rsr0012@uah.edu>
2025-11-10 12:44:31 -0600

    work in progress for lsp mode

--- modules/personal/lsp_mode.el
+++ modules/personal/lsp_mode.el
@@ -1,47 +1,40 @@
+;; LSP MODE (Core)
 (use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c v")
+  :ensure t
   :commands (lsp lsp-deferred)
-  :hook (lsp-mode . lsp-enable-which-key-integration)
+  :hook (;; JavaScript and TypeScript
+	 (js-mode . lsp-deferred)
+	 (typescript-mode . lsp-deferred)
+	 ;; JSON
+	 (json-mode . lsp-deferred)
+	 ;; HTML, CSS, SCSS, Less
+	 (html-mode . lsp-deferred)
+	 (css-mode . lsp-deferred)
+	 (scss-mode . lsp-deferred)
+	 (less-mode . lsp-deferred)
+	 ;; YAML
+	 (yaml-mode . lsp-deferred))
+  :init
+  ;; This sets `lsp-mode` to use `flycheck` for displaying errors
+  (setq lsp-diagnostics-provider :flycheck)
   :config
-  (setq lsp-auto-guess-root t)
-  (setq lsp-project-root-patterns
-  '("package.json" "tsconfig.json" "jsconfig.json" ".git" ".project" ".projectile"))
+  ;; This enables ESLint as a linter within lsp-mode
+  (setq lsp-eslint-enable t)
+  ;;
+  :custom
+  (lsp-language-id-configuration '((rjsx-mode . "javascriptreact")))
+  (lsp-typescript-suggest-auto-imports t)
+  (lsp-typescript-format-enable t)
+  (lsp-typescript-preferences-quote-style "single"))
 
-  ;; PERFORMANCE
-  (setq lsp-completion-provider :capf
-  lsp-idle-delay 0.1
-  lsp-document-sync-method 'incremental
-  lsp-response-timeout 10
-  lsp-enable-file-watchers t
-  lsp-file-watch-threshold 5000)
 
-  ;; INTEGRATIONS
-  (setq lsp-eslint-enable t))
-  (setq lsp-format-buffer-on-save nil)
-  ;; This sets the default for all buffers to use spaces instead of tabs.
-  (setq-default indent-tabs-mode nil)
-
-  ;; This sets the visual width of a tab to 2 spaces. This is useful
-  ;; for files that might still contain tab characters.
-  (setq-default tab-width 2)
-
+;; LSP UI (UI Enhancements)
 (use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :bind
-  (:map lsp-ui-mode-map
-  ("C-c v d" . lsp-ui-doc-show))
+  :ensure t
+  :commands lsp-ui-mode
+  :after lsp-mode
   :config
-  ;; DOCUMENTATION SETTINGS
-  (setq lsp-ui-doc-show-with-mouse nil)
-  (setq lsp-ui-sideline-show-hover nil)
-  (setq lsp-ui-doc-position 'bottom)
-  (setq lsp-ui-doc-frame-size '(0.3 . 1))
-
-  ;; OTHER UI FEATURES
-  (setq lsp-ui-peek-enable t
-  lsp-ui-imenu-enable t
-  lsp-ui-sideline-enable t
-  lsp-ui-sideline-delay 0.1))
-
-(provide 'lsp-config)
+  ;; Customizations for the UI
+  (setq lsp-ui-doc-position 'at-point) ; Show documentation popup at the cursor
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq lsp-ui-sideline-show-hover t))
--- modules/personal/webdev-config.el
+++ modules/personal/webdev-config.el
@@ -16,31 +16,10 @@
   :config
   (setq typescript-indent-level 2))
 
-
-;; Tell LSP how to handle JSX syntax
-(with-eval-after-load 'lsp-mode
-  (add-to-list 'lsp-language-id-configuration '(rjsx-mode . "javascriptreact"))
-  (setq lsp-typescript-suggest-auto-imports t
-  lsp-typescript-format-enable t
-  lsp-typescript-preferences-quote-style "single"))
-
 ;; Development tools
 (use-package add-node-modules-path
   :hook ((rjsx-mode . add-node-modules-path)
    (typescript-mode . add-node-modules-path)))
-
-(use-package prettier-js)
-(defun my-add-prettier-keybinding ()
-  "Add keybinding for manual Prettier formatting."
-  (local-set-key (kbd "M-F") #'prettier-js))
-(add-hook 'rjsx-mode-hook #'my-add-prettier-keybinding)
-(add-hook 'typescript-mode-hook #'my-add-prettier-keybinding)
-
-(use-package emmet-mode
-  :hook ((rjsx-mode . emmet-mode)
-   (typescript-mode . emmet-mode)
-   (css-mode . emmet-mode))
-  :config (setq emmet-expand-jsx-className? t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; YAML AND .ENV FILE SUPPORT
@@ -56,65 +35,30 @@
   :ensure t
   :mode "\\.env\\'")
 
-;; B. Enable Autocompletion for YAML
-;; This hooks lsp-mode into yaml-mode. It will automatically start the
-;; yaml-language-server (if you installed it via npm).
-(add-hook 'yaml-mode-hook #'lsp-deferred)
-
-
-(add-hook 'rjsx-mode-hook #'lsp-deferred)
-(add-hook 'typescript-mode-hook #'lsp-deferred)
-(add-hook 'js-mode-hook #'lsp-deferred)
 ;; React snippets and helpers
 (use-package react-snippets :after yasnippet)
 
-(defun my/insert-react-component (name)
-  "Insert a React functional component template."
-  (interactive "sComponent name: ")
-  (insert (format "import React from 'react';
 
-const %s = () => {
-  return (
-    <div>
-      <h1>%s Component</h1>
-    </div>
-  );
-};
+(defun my-dev-mode-setup ()
+  "Unified setup for all web development modes."
+  ;; Enable major tools
+  (lsp-deferred)
+  (emmet-mode)
+  (add-node-modules-path)
 
-export default %s;" name name name)))
-
-(defun my/insert-react-hook (hook-name)
-  "Insert a custom React hook template."
-  (interactive "sHook name (without 'use' prefix): ")
-  (let ((full-hook-name (if (string-prefix-p "use" hook-name) hook-name
-        (concat "use" (capitalize hook-name)))))
-    (insert (format "import { useState, useEffect } from 'react';
-
-const %s = () => {
-  const [state, setState] = useState();
-
-  useEffect(() => {
-    // Effect logic here
-  }, []);
-
-  return state;
-};
-
-export default %s;" full-hook-name full-hook-name))))
-
-(global-set-key (kbd "C-c r c") 'my/insert-react-component)
-(global-set-key (kbd "C-c r h") 'my/insert-react-hook)
-
-;; LSP navigation keybindings for web modes
-(defun my-dev-mode-setup-keys ()
-  "Set up keybindings for LSP/xref navigation in dev modes."
+  ;; Set up keybindings locally for this buffer
   (define-key (current-local-map) (kbd "M-.") #'lsp-find-definition)
   (define-key (current-local-map) (kbd "M-,") #'xref-pop-marker-stack)
   (define-key (current-local-map) (kbd "M-?") #'lsp-find-references)
-  (define-key (current-local-map) (kbd "C-M-.") #'lsp-find-implementation))
+  (define-key (current-local-map) (kbd "C-M-.") #'lsp-find-implementation)
+  ;; Use LSP for formatting instead of a separate package
+  (define-key (current-local-map) (kbd "M-F") #'lsp-format-buffer))
+
+
 
 (add-hook 'rjsx-mode-hook #'my-dev-mode-setup-keys)
 (add-hook 'typescript-mode-hook #'my-dev-mode-setup-keys)
+(add-hook 'yaml-mode-hook #'my-dev-mode-setup-keys)
 
 ;; This line is crucial for modular configurations
 (provide 'web-dev-config)
