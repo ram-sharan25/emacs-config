dba81b90b2a5a7dcab34d4ffe54612d2abbfa214
Ram Sharan Rimal <rsr0012@uah.edu>
2025-11-24 13:40:23 -0600

    change the sync for rclone

--- modules/personal/rclone.el
+++ modules/personal/rclone.el
@@ -1,10 +1,11 @@
+(require 'paths)
 (defun sync-notes-with-rclone ()
   "Sync notes directory to a designated remote with rclone."
   (interactive)
   (message "Syncing notes with rclone...")
   ;; Use start-process for background execution without capturing output
   (start-process "rclone-sync" nil "rclone" "copy"
-                 (expand-file-name "~/Stillness/Personal/")
+                 (expand-file-name my/brain-dir)
                  "One Drive:Personal/")
   (message "Sync initiated in background."))
 
