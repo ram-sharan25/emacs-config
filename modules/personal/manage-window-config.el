aec4cac8b2b9eb52dfa78e5ae694e0e511fc8138
Ram Sharan Rimal <rsr0012@uah.edu>
2025-11-30 20:42:56 -0600

    add vertico and marginalia

--- modules/personal/manage-window-config.el
+++ modules/personal/manage-window-config.el
@@ -15,3 +15,22 @@
   (scratch-buffer))
 
 (global-set-key (kbd "C-x 2") 'rsr/split-horizontal)
+
+
+;; Enable Vertico.
+(use-package vertico
+  :custom
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+   (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+
+
+(use-package marginalia
+  :ensure t
+  :custom
+  (marginalia-max-relative-age 0) ;; Example: customize a specific variable here
+  :init
+  (marginalia-mode))
