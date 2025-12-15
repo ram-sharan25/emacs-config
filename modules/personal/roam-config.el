94aad9032c9bba69618054671d90c5be26a214a0
Ram Sharan Rimal <rsr0012@uah.edu>
2025-12-15 11:46:03 -0600

    fix roam config

--- modules/personal/roam-config.el
+++ modules/personal/roam-config.el
@@ -3,18 +3,18 @@
 (use-package org-roam
   :ensure t
   :bind (:map global-map
-	(("M-m r f" . org-roam-node-find)
-	 ("M-m r i" . org-roam-node-insert)
-	 ("M-m r F" . org-roam-ref-find)
-	 ("M-m r c" . org-roam-capture)
-	 ("M-m r g" . org-roam-graph)))
+        (("M-m r f" . org-roam-node-find)
+         ("M-m r i" . org-roam-node-insert)
+         ("M-m r F" . org-roam-ref-find)
+         ("M-m r c" . org-roam-capture)
+         ("M-m r g" . org-roam-graph)))
   :bind (:map org-mode-map
-	(("M-m o r" . org-roam-node-insert)
-	 ("M-m r i" . org-roam-node-insert)
-	 ("M-m r r" . org-roam-buffer-toggle)
-	 ("M-m r R" . org-roam-ref-add)
-	 ("M-m r a" . org-roam-alias-add)
-	 ("M-m r t" . org-roam-tag-add)))
+        (("M-m o r" . org-roam-node-insert)
+         ("M-m r i" . org-roam-node-insert)
+         ("M-m r r" . org-roam-buffer-toggle)
+         ("M-m r R" . org-roam-ref-add)
+         ("M-m r a" . org-roam-alias-add)
+         ("M-m r t" . org-roam-tag-add)))
   :config
   (setq org-roam-directory my/brain-dir)
   (setq org-roam-file-extensions '("org" "org_archive"))
@@ -24,14 +24,12 @@
 
   ;; Link Abbreviation for portable data links
   ;; Usage: [[data:image.png]] -> expands to my/data-dir/image.png
-  (setq org-link-abbrev-alist
-	(list (cons "data" (concat my/data-dir "%s"))))
 
   (setq org-roam-capture-templates
   '(("z" "Zettel" plain
      "%?"
      :if-new (file+head "Public/${slug}.org"
-	    "#+title: ${title}\n#+filetags: :ZETTEL:\n#+AREA: %(progn (setq my/zettel-area (my/select-area-default-misc)) my/zettel-area)\n#+PROJECT: %(let* ((proj-cons (my/org-select-project-allow-empty my/zettel-area)) (proj-name (car proj-cons)) (proj-id (cdr proj-cons))) (if proj-id (format \"[[id:%s][%s]]\" proj-id proj-name) proj-name))\n\n* Context\n- Area: [[id:%(my/get-area-id-by-name my/zettel-area)][%(identity my/zettel-area)]]\n")
+            "#+title: ${title}\n#+filetags: :ZETTEL:\n#+AREA: %(progn (setq my/zettel-area (my/select-area-default-misc)) my/zettel-area)\n#+PROJECT: %(let* ((proj-cons (my/org-select-project-allow-empty my/zettel-area)) (proj-name (car proj-cons)) (proj-id (cdr proj-cons))) (if proj-id (format \"[[id:%s][%s]]\" proj-id proj-name) proj-name))\n\n* Context\n- Area: [[id:%(my/get-area-id-by-name my/zettel-area)][%(identity my/zettel-area)]]\n")
      :unnarrowed t)))
 
   (setf org-id-link-to-org-use-id t))
