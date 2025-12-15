363ced327178361866ac8edc9552340e856a4d75
Ram Sharan Rimal <rsr0012@uah.edu>
2025-12-15 11:44:50 -0600

    add download video

--- modules/personal/mpv-notes.el
+++ modules/personal/mpv-notes.el
@@ -13,3 +13,38 @@
 
 (use-package mpv
   :ensure t)
+
+;; configure .mp4 files to open with 'mpv' automatically
+(add-to-list 'org-file-apps '("\\.mp4\\'" . "mpv \"%s\""))
+
+(defun my/download-video (link-key filename url)
+  "Download video to the directory defined by LINK-KEY in `org-link-abbrev-alist`.
+Example: Enter 'dsa_dir' to download to the DSA lectures folder."
+  (interactive
+   (let ((keys (mapcar #'car org-link-abbrev-alist)))
+     (list (completing-read "Target Directory Key (e.g., dsa_lec): " keys)
+           (read-string "Enter filename (without extension): ")
+           (read-string "Enter URL: "))))
+
+  (let* ((expansion (cdr (assoc link-key org-link-abbrev-alist)))
+         ;; The expansion looks like "~/path/to/lectures/%s".
+         ;; We replace "%s" with empty string to get the base directory.
+         (clean-path (replace-regexp-in-string "%s" "" expansion))
+         (target-dir (file-name-as-directory (expand-file-name clean-path))))
+
+    ;; Create directory if it doesn't exist (safety check)
+    (unless (file-exists-p target-dir)
+      (make-directory target-dir t))
+
+    ;; Construct and run the command
+    (let ((default-directory target-dir)
+          (cmd (format "yt-dlp -o '%s/%%(title)s.%%(ext)s' -o '%s/%s.%%(ext)s' '%s' --cookies-from-browser chrome"
+                       target-dir
+                       target-dir
+                       filename
+                       url)))
+      (message "Downloading to: %s" target-dir)
+      (async-shell-command cmd (format "*yt-dlp: %s*" filename)))))
+
+(setq org-link-abbrev-alist
+      (list (cons "dsa_lec" (concat my/dsa-lectures "%s"))))
