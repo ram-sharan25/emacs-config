18b552dd0edc1a6585d3df909312f7b995dbad9f
Ram Sharan Rimal <rsr0012@uah.edu>
2025-12-15 11:16:20 -0600

    add gptel config

---
+++ modules/personal/gptel-config.el
@@ -0,0 +1,15 @@
+(use-package gptel
+  :ensure t
+  :bind (:map rsr/global-prefix-map
+              (("g g" . gptel-send)
+               ("g b" . gptel)
+               ("g f" . gptel-add-file)
+               ("g a" . gptel-context-add)
+               ("g r" . gptel-context-remove-all)
+               ("g q" . gptel-abort)))
+  :config
+  ;; Set the api key in .authinfo file like:
+  ;; machine generativelanguage.googleapis.com apikey password <<YOUR_API_KEY>>
+  (setf gptel-default-mode 'org-mode
+        gptel-model 'gemini-flash-latest
+        gptel-backend (gptel-make-gemini "Gemini" :stream t :key "AIzaSyC0mCZbQ2Q6msc2Fc7Z30qnJm18GNlMHTE")))
