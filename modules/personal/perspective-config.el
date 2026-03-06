29ca925d8cf017d4b5e70ba042a3aeff4bfdfdbe
Ram Sharan Rimal <rsr0012@uah.edu>
2026-03-06 02:29:55 -0600

    add perspective in opening new project

--- modules/personal/perspective-config.el
+++ modules/personal/perspective-config.el
@@ -1,7 +1,30 @@
+;;; perspective-config.el --- Named workspace perspectives -*- lexical-binding: t; -*-
+
+;;; Code:
+
 (use-package perspective
-  :bind
-  ("C-x C-b" . persp-list-buffers)         ; or use a nicer switcher, see below
+  :ensure t
   :custom
-  (persp-mode-prefix-key (kbd "C-c C-p"))  ; pick your own prefix key here
+  (persp-mode-prefix-key (kbd "C-c C-p"))
   :init
   (persp-mode))
+
+(use-package persp-projectile
+  :ensure t
+  :after (perspective projectile)
+  :bind (:map projectile-mode-map
+         ("C-c p p" . projectile-persp-switch-project)))
+
+;;; --- Commands ---
+
+(defun rsr/open-project-in-new-frame ()
+  "Open a projectile project in a new frame with its own perspective."
+  (interactive)
+  (select-frame (make-frame))
+  (run-with-idle-timer 0.1 nil #'call-interactively #'projectile-persp-switch-project))
+
+;;; Keybindings
+(global-set-key (kbd "C-c C-p F") #'rsr/open-project-in-new-frame)
+
+(provide 'perspective-config)
+;;; perspective-config.el ends here
