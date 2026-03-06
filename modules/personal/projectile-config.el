ccdfbfad2e3c87596717bd3660753ddd4db135bf
Ram Sharan Rimal <rsr0012@uah.edu>
2026-03-06 02:38:06 -0600

    fix projectile mode

--- modules/personal/projectile-config.el
+++ modules/personal/projectile-config.el
@@ -1,10 +1,14 @@
+;;; projectile-config.el --- Project navigation -*- lexical-binding: t; -*-
+
+;;; Code:
+
 (use-package projectile
   :ensure t
   :init
-  ;; This line enables projectile globally when Emacs starts.
   (projectile-mode +1)
-  :config
-  ;; These keybindings will now be set correctly after projectile is loaded.
-  ;; The `s-` prefix is for the Super key (Windows or Command keys).
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  :bind (:map projectile-mode-map
+         ("s-p"   . projectile-command-map)
+         ("C-c p" . projectile-command-map)))
+
+(provide 'projectile-config)
+;;; projectile-config.el ends here
